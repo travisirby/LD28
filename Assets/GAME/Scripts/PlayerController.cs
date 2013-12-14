8cf109ab25c5b35e51d713fa791aea7cd0bc3254
Travis Irby <travis.irby@gmail.com>
2013-12-14 16:04:01 -0500

    harpoon angle working

--- Assets/GAME/Scripts/PlayerController.cs
+++ Assets/GAME/Scripts/PlayerController.cs
@@ -37,10 +37,8 @@ using System.Collections;
 		public LayerMask groundLayers;						// These layers will be included in the linecasts to test if grounded
 		public float groundLinecastLength = 1f;
 
-		private Transform[] groundCastChilds;
 		private Animator animator;
 		private AnimHashIDs animHashIDs;
-		private RaycastHit2D groundLinecast;
 		private Rigidbody2D rb2D;
 		private int onJump;
 		private float speedMode, hInput, jumpTime, slope;
@@ -64,20 +62,15 @@ using System.Collections;
 				rb2D = rigidbody2D;															// Cache rigidbody2D for performance reasons
 		//		animator = transform.Find ("Boy").gameObject.GetComponent<Animator>();		// Get Boy's Animator to change animations
 		//		animHashIDs = transform.Find("Boy").gameObject.GetComponent<AnimHashIDs>();
-
-				groundCasts = transform.Find ("GroundCasts");								// Find our groundCasts child gameobject
-
-				groundCastChilds = new Transform[groundCasts.childCount];	// Create a Transform array and find all groundCasts children 
-				for (int i=0; i < groundCastChilds.Length; i++)				// Used for linecasting to see if we are grounded
-					groundCastChilds[i] = groundCasts.GetChild(i);
 			}
+
 		}
 
 		void Update()
 		{	
 			if (isThisMyObject)
 			{
-				JumpSetup ();
+				
 
 				//get horizontal input
 				hInput = Input.GetAxisRaw ("Horizontal");
@@ -100,120 +93,42 @@ using System.Collections;
 		{
 			if (isThisMyObject)
 			{
-				// grounded test
-				grounded = CheckIfGrounded ();
-				//AnimatorStateInfo currentAnimState = animator.GetCurrentAnimatorStateInfo(0);
 
-//				if (grounded)
-//				{
-					//animator.SetBool(animHashIDs.grounded, true);
-	//				if (currentAnimState.nameHash == animHashIDs.fallState && !crossFadingLanding)
-	//				{
-	//					CancelInvoke ("ResetCrossFadingLanding");
-	//					crossFadingLanding = true;
-	//					//animator.CrossFade (animHashIDs.landState,0.05f,0,Time.deltaTime);
-	//					Invoke ("ResetCrossFadingLanding",0.15f);
-	//				}
-					speedMode = movementProps.moveSpeed;
+				JumpSetup ();
 
-					if (isJumpingDelayed)		// If we have just landed from a jump
-					{
-			
-						if (Mathf.Abs (hInput) > 0.5f) // If the player is applying hInput, add force in the jump direction
-						{
-							rb2D.AddForce(new Vector2 (jumpProps.landingSlideAmount * hInput, groundLinecast.normal.x * jumpProps.landingSlideYForce));
-							tnSyncPlayer.Sync();
-						}
-						// Reset jump bool variables
-						isJumping = false;
-						isJumpingDelayed = false;
-					}
+				speedMode = movementProps.moveSpeed;
 
-					groundedDelayCheck = true;			// groundedDelayCheck is used to allow a forgiving delayed jump
-//				}
-//				else // not grounded
-//				{
-	//				if (currentAnimState.nameHash != animHashIDs.fallState && !isJumping && !crossFadingFalling)
-	//				{
-	//					CancelInvoke ("ResetCrossFadingFalling");
-	//					crossFadingFalling = true;
-	//					//animator.CrossFade (animHashIDs.fallState,0.1f,0,Time.deltaTime);
-	//					Invoke ("ResetCrossFadingFalling",0.15f);
-	//				}
-					//animator.SetBool(animHashIDs.grounded, false);
-//					speedMode = movementProps.airMoveSpeed;
-//					slope = 0f;
-//
-//					if (groundedDelayCheck && !groundedDelayCheckReset)			// Reset groundedDelayCheck (used for forgiving jumps)
-//					{
-//						groundedDelayCheckReset = true;
-//						Invoke("GroundedDelayCheckReset", jumpProps.jumpCheckTime);	
-//					}
-//				}
-
-
-				// Moving platform support
-				Vector2 movingPlatformSpeed = Vector2.zero;
-				if (grounded)
+				if (isJumpingDelayed)		// If we have just landed from a jump
 				{
-					if (groundLinecast.transform.CompareTag("MovingPlatform"))
+			
+					if (Mathf.Abs (hInput) > 0.5f) // If the player is applying hInput, add force in the jump direction
 					{
-						isOnMovingPlatform = true;
-						movingPlatformSpeed = groundLinecast.transform.rigidbody2D.velocity;
-						Vector2 newVel = new Vector2(movingPlatformSpeed.x * movementProps.movingPlatformFriction * Time.deltaTime, 0f);
-						rb2D.velocity = new Vector2 (newVel.x, rb2D.velocity.y);
-
+						rb2D.AddForce(new Vector2 (jumpProps.landingSlideAmount * hInput, jumpProps.landingSlideYForce));
+						tnSyncPlayer.Sync();
 					}
-					else
-						isOnMovingPlatform = false;
-
+					// Reset jump bool variables
+					isJumping = false;
+					isJumpingDelayed = false;
 				}
+
+				groundedDelayCheck = true;			// groundedDelayCheck is used to allow a forgiving delayed jump
 
 
 				float moveTo = transform.position.x + hInput;
 
-				Move (moveTo, speedMode, slope,  0.7f);	// Tested stopDistance, 0.7f is the best
+				Move (moveTo, speedMode, 0f,  0.7f);	// Tested stopDistance, 0.7f is the best
 
 				if (!isJumpingDelayed) 
-					LimitSpeed (movementProps.maxSpeedXGround + movingPlatformSpeed.magnitude, movementProps.maxSpeedY);
+					LimitSpeed (movementProps.maxSpeedXGround, movementProps.maxSpeedY);
 				else 
-					LimitSpeed (movementProps.maxSpeedXAir + movingPlatformSpeed.magnitude, movementProps.maxSpeedY);
+					LimitSpeed (movementProps.maxSpeedXAir, movementProps.maxSpeedY);
 
-	//			if (!hInputActivated) SlideCheck();
 
-				//RotateToGround();
 			}
 		}
 
 		void ResetHInputActivated () { hInputActivated = false; }
 
-		void GroundedDelayCheckReset ()			// Called to reset groundedDelayCheck after we have been not grounded 
-		{										// for jumpProps.jumpCheckTime (used so you can jump a little bit after you are not grounded)
-			groundedDelayCheck = false;
-			groundedDelayCheckReset = false;
-		}
-
-		private bool CheckIfGrounded() 		// Returns true if groundLineCast hits a collider2D which is in the groundLayers layermask.
-		{
-			// Check if we are grounded by linecasting from each groundCast position
-			for (int i = 0; i < groundCastChilds.Length; i++)
-			{
-				Vector3 linecastStopPos = new Vector3 (groundCastChilds[i].localPosition.x, groundCastChilds[i].localPosition.y - groundLinecastLength, 0f);
-				groundLinecast = Physics2D.Linecast(groundCastChilds[i].position, transform.TransformPoint(linecastStopPos), groundLayers);
-
-				Debug.DrawLine(groundCastChilds[i].position, transform.TransformPoint(linecastStopPos), Color.red,2f);
-
-				if (groundLinecast)
-				{
-					//find the slope of the collider we are on
-					slope = groundLinecast.normal.normalized.x;
-					// yes grounded
-					return true;
-				}
-			}
-			// not grounded
-			return false;
-		}
 
 		private void Move(float finishPosition, float speed, float tempSlope, float stopDistance)
 		{
@@ -237,31 +152,13 @@ using System.Collections;
 		//jumping
 		private void JumpSetup()
 		{
-			//if we press jump in the air, save the time
-			if (Input.GetButtonDown ("Jump") && !grounded)
-				jumpTime = Time.time;
-			
-			//if were on ground within slope limit
-			if (grounded || groundedDelayCheck)
-			{
-				if (Mathf.Abs (slope) < movementProps.slopeLimit)
-				{
-					//and we press jump, or we pressed jump justt before hitting the ground
-					if (Input.GetButtonDown ("Jump") || jumpTime + jumpProps.jumpLeniancy > Time.time)
-					{	
-						groundedDelayCheck = false;
-						groundedDelayCheckReset = false;
-						Jump (jumpProps.jumpForce);
-
-					}
-				}
-			}
+			if (Input.GetButton ("Jump"))
+				Jump (jumpProps.jumpForce);
 		}
 		
 		//push player at jump force
 		public void Jump(Vector3 jumpVelocity)
 		{
-			isJumping = true;
 
 			//animator.SetBool(animHashIDs.jumping,true);
 		//	animator.Play (animHashIDs.jumpState);
@@ -272,20 +169,14 @@ using System.Collections;
 
 			if (Mathf.Abs (hInput) > 0.1f) jumpVelocity.x = hInput * jumpProps.jumpInputStrength;	// If there's horizontal input (h) add it to jump force
 
-			if (groundLinecast.normal.x > 0 && rb2D.velocity.x < 0 || groundLinecast.normal.x < 0 && rb2D.velocity.x > 0)
-			{
-				jumpVelocity.y = jumpVelocity.y + Mathf.Abs (groundLinecast.normal.x) * jumpProps.angledJumpYAddedForce;
-			}
-
 			rb2D.AddForce(jumpVelocity);
-			tnSyncPlayer.Sync();
+	//		tnSyncPlayer.Sync();
 			jumpTime = 0f;
-			Invoke("SetIsJumpingDelayed", 0.2f);
+
 		}
 
 		//void ResetAnimJumpingBool () { animator.SetBool(animHashIDs.jumping,false); }
 
-		void SetIsJumpingDelayed () { isJumpingDelayed = true; }
 
 		void SlideCheck ()
 		{
@@ -297,34 +188,34 @@ using System.Collections;
 			}
 		}
 
-		void RotateToGround ()
-		{
-			Quaternion fromRotation = transform.rotation;
-			Quaternion toRotation;
-			if (groundLinecast)
-			{
-				if (Mathf.Abs (slope) < movementProps.slopeLimit)
-				{
-					toRotation = Quaternion.FromToRotation(Vector2.up, groundLinecast.normal);
-					float weight = 0f;
-					if(weight <= 1f) 
-					{
-						weight += Time.deltaTime * 10f;
-						transform.rotation = Quaternion.Slerp(fromRotation, toRotation, weight);
-					}
-				}
-			}
-			else 
-			{
-				toRotation = Quaternion.identity;
-				float weight = 0f;
-				if(weight <= 1f) 
-				{
-					weight += Time.deltaTime * 2f;
-					transform.rotation = Quaternion.Slerp(fromRotation, toRotation, weight);
-				}
-			}
-		}
+//		void RotateToGround ()
+//		{
+//			Quaternion fromRotation = transform.rotation;
+//			Quaternion toRotation;
+//			if (groundLinecast)
+//			{
+//				if (Mathf.Abs (slope) < movementProps.slopeLimit)
+//				{
+//					toRotation = Quaternion.FromToRotation(Vector2.up, groundLinecast.normal);
+//					float weight = 0f;
+//					if(weight <= 1f) 
+//					{
+//						weight += Time.deltaTime * 10f;
+//						transform.rotation = Quaternion.Slerp(fromRotation, toRotation, weight);
+//					}
+//				}
+//			}
+//			else 
+//			{
+//				toRotation = Quaternion.identity;
+//				float weight = 0f;
+//				if(weight <= 1f) 
+//				{
+//					weight += Time.deltaTime * 2f;
+//					transform.rotation = Quaternion.Slerp(fromRotation, toRotation, weight);
+//				}
+//			}
+//		}
 
 		public void LimitSpeed(float maxSpeedX, float maxSpeedY)
 		{	
--- Assets/ThrowHarpoon.cs
+++ Assets/ThrowHarpoon.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class ThrowHarpoon : MonoBehaviour {
 
-	public GameObject harpoonPrefab;
-	public Transform sprite;
+	public float harpoonForce = 200;
 
-	private bool isThisMyObject;
+	private bool isReady, isThisMyObject, holdingHarpoon = true, throwingHarpoon;
 
 	void Awake()
 	{
@@ -15,16 +14,71 @@ public class ThrowHarpoon : MonoBehaviour {
 			isThisMyObject = true;
 		}
 	}
+
+	void Start ()
+	{
+		Invoke("SetIsReady",2f);
+	}
+
+	void SetIsReady () { isReady = true; }
 	
 	void Update ()
 	{
-		if(isThisMyObject && Input.GetMouseButtonDown(0))
+		if(Input.GetMouseButtonDown(0) && isReady && isThisMyObject && holdingHarpoon)
 		{
-			Debug.Log ("Clicked");
-			Vector3 clickDistance = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-			Quaternion clickRotate = Quaternion.Euler(clickDistance.normalized);
-			GameManager.Instance.CreateHarpoon (harpoonPrefab, transform.position, Quaternion.LookRotation(clickDistance.normalized,Vector3.right), Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position, false);
-	//		TNManager.Create(harpoonPrefab, transform.position, Quaternion.identity, false);
+			throwingHarpoon = true;
+			holdingHarpoon = false;
+			Invoke ("ResetThrowingHarpoon", 3f);
+
+			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector3 clickDistance = mouseWorldPos - transform.position;
+
+			transform.parent = null;
+//
+//			Vector3 upAxis = new Vector3(0,0,1);
+//			Vector3 mouseScreenPosition = Input.mousePosition;
+//			//set mouses z to your targets
+//			mouseScreenPosition.z = transform.position.z;
+//			Vector3 mouseWorldSpace = Camera.mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+//			transform.LookAt(mouseWorldSpace, upAxis);
+//			//zero out all rotations except the axis I want
+//			transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z);
+
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = 10f; //The distance from the camera to the player object
+			Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+			lookPos = lookPos - transform.position;
+			float angle = Mathf.Atan2(lookPos.y, lookPos.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+			//var angle = Mathf.Atan2(Input.mousePosition.x, Input.mousePosition.y) * Mathf.Rad2Deg;
+
+			//transform.localEulerAngles = new Vector3 (0f, 0f, angle);
+
+			rigidbody2D.AddForce( clickDistance.normalized * harpoonForce);
+
+		}
+	}
+
+	void ResetThrowingHarpoon () { throwingHarpoon = false; }
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+
+		if (col.CompareTag("Player"))
+		{
+			if (isThisMyObject && !holdingHarpoon && !throwingHarpoon)
+			{
+				Debug.Log("mine");
+				holdingHarpoon = true;
+				transform.parent = col.transform;
+				transform.localPosition = Vector3.zero;
+				transform.rotation = Quaternion.identity;
+			}
+			else if (!isThisMyObject)
+			{
+				Debug.Log("hit");
+			}
 		}
 	}
 }
