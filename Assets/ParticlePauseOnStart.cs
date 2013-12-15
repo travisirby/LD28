b3695ee92f9d6110d6506768dc23080ad1ea7af9
Travis Irby <travis.irby@gmail.com>
2013-12-15 04:07:19 -0500

    sprite work, graphics, particles

---
+++ Assets/ParticlePauseOnStart.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticlePauseOnStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("PauseParticle", 1f);
+	}
+	
+	void PauseParticle ()
+	{
+		particleSystem.Pause ();
+	}
+}
